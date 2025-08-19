269c7ae0c02c26acf127e90d63761875bb422d1e
Phil <182430206+Fico27@users.noreply.github.com>
2025-08-19 13:14:39 +0200

    Added in message in the index router to be displayed by default.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,24 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hey, I'll see you at the party later!",
+    user: "Ana",
+    added: new Date(),
+  },
+  {
+    text: "OMW to pick you up! Don't forget the beer!",
+    user: "Jeff",
+    added: new Date(),
+  },
+  {
+    text: "I'm waiting outside. Lets roll!",
+    user: "Jeff",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) =>
   res.send("This is the index with all current messages!")
 );
