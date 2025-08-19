73bd098591a9bd000e3c3fff238c9eb55a255258
Phil <182430206+Fico27@users.noreply.github.com>
2025-08-19 15:12:31 +0200

    added in the ability to create and submit a new message. Data handling still needs to be fixed when pdating messages in indexRouter.

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,6 +1,17 @@
 const { Router } = require("express");
 const newRouter = Router();
+const messages = require("./indexRouter");
 
 newRouter.get("/", (req, res) => res.render("form.ejs"));
-
+newRouter.post("/", (req, res) => {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
+});
 module.exports = newRouter;
+
+//FIX HOW MESSAGE DATA IS UPDATED
