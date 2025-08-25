40558d22260b843de501ab08e5bef5831438edc2
Phil <182430206+Fico27@users.noreply.github.com>
2025-08-25 13:55:19 +0200

    changed controler capability to work with railway db.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,25 +1,44 @@
 //controller
+const db = require("../db/query");
 
 async function getMessage(req, res) {
-  const messageId = parseInt(req.params.id);
-  res.render("message.ejs", {
-    title: "Message Details",
-    message: messages[messageId],
-  });
+  try {
+    const messageId = parseInt(req.params.id);
+    const message = await db.getMessageById(messageId);
+
+    if (!message) {
+      return res.status(400).send("Message not found!");
+    }
+
+    res.render("message.ejs", {
+      title: "Message Details",
+      message: messages[messageId],
+    });
+  } catch (error) {
+    console.error("Error getting message:", error);
+    res.status(500).send("Error getting message");
+  }
 }
 
 async function getIndex(req, res) {
-  res.render("index.ejs", { title: "Mini Messageboard", messages: messages });
+  try {
+    const messages = await db.getAllMessages();
+    res.render("index.ejs", { title: "Mini Messageboard", messages: messages });
+  } catch (error) {
+    console.error("Could not find messages:", error);
+    res.status(500).send("Could not find messages!");
+  }
 }
 
 async function createNewMessagePost(req, res) {
-  const newMessage = {
-    text: req.body.message,
-    user: req.body.user,
-    added: new Date(),
-  };
-  messages.push(newMessage);
-  res.redirect("/");
+  try {
+    const { message: text, user } = req.body;
+    await db.insertMessage(text, user);
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error writing new message", error);
+    res.status(500).send("Error writing new message.");
+  }
 }
 
 module.exports = {
