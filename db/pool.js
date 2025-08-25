4a394e522f27b2738b0d287a4f23a11e8c578682
Phil <182430206+Fico27@users.noreply.github.com>
2025-08-25 15:50:38 +0200

    Updated pool to fix db issues.

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,25 @@
 //pool.js
 
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// module.exports = new Pool({
+//   connectionString: process.env.DATABASE_URL || process.env.DATABASE_PRIVATE_URL
+//   ssl: { rejectUnauthorized: false },
+// });
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString =
+  process.env.DATABASE_URL || process.env.DATABASE_PRIVATE_URL;
+
+if (!connectionString) {
+  throw new Error("No DATABASE_URL or DATABASE_PRIVATE_URL provided");
+}
+
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
