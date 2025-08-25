a13798bc7a7646c94861c1df1d6741107c17321f
Phil <182430206+Fico27@users.noreply.github.com>
2025-08-25 11:19:42 +0200

    added in a controller file to the project. Also installed dotenv.

---
+++ controllers/controller.js
@@ -0,0 +1,27 @@
+async function getMessage(req, res) {
+  const messageId = parseInt(req.params.id);
+  res.render("message.ejs", {
+    title: "Message Details",
+    message: messages[messageId],
+  });
+}
+
+async function getIndex(req, res) {
+  res.render("index.ejs", { title: "Mini Messageboard", messages: messages });
+}
+
+async function createNewMessagePost(req, res) {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
+}
+
+module.exports = {
+  getMessage,
+  getIndex,
+  createNewMessagePost,
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const indexRouter = Router();
+const messageController = require("../controllers/controller");
 
 const messages = [
   {
@@ -19,26 +20,10 @@ const messages = [
   },
 ];
 
-indexRouter.get("/message/:id", (req, res) => {
-  const messageId = parseInt(req.params.id);
-  res.render("message.ejs", {
-    title: "Message Details",
-    message: messages[messageId],
-  });
-});
+indexRouter.get("/message/:id", messageController.getMessage);
 
-indexRouter.get("/", (req, res) =>
-  res.render("index.ejs", { title: "Mini Messageboard", messages: messages })
-);
+indexRouter.get("/", messageController.getIndex);
 
-indexRouter.post("/", (req, res) => {
-  const newMessage = {
-    text: req.body.message,
-    user: req.body.user,
-    added: new Date(),
-  };
-  messages.push(newMessage);
-  res.redirect("/");
-});
+indexRouter.post("/", messageController.createNewMessagePost);
 
 module.exports = indexRouter;
